4174ee511623f847e60323856cd7da1778a61ed4
gutao <gutaoai@126.com>
2015-09-14 15:09:01 +0800

    gutao

--- src/main/java/gt/controller/apply/TestController.java
+++ src/main/java/gt/controller/apply/TestController.java
@@ -2,13 +2,23 @@ package gt.controller.apply;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+
 public class TestController {
 	 @RequestMapping(value = "index")
-    public String test(){
+     public String test(){
 		 System.out.println();
     	 return "index";
      }
+	 @RequestMapping(value = "show")
+	    public ModelAndView add(String username){
+		     ModelAndView user = new ModelAndView("index");
+		     user.addObject("name",username);
+		     user.addObject("id","1212");
+			 System.out.println("--------test---------");
+	    	 return user;
+	     }
 }
---
+++ target/ShopSpringMVC/WEB-INF/pages/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isELIgnored="false"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/ShopSpringMVC/show.html" method="post">
+  用户名：<input name="username" type="text"/>
+  <input type="submit" value="提交"/>
+  获取数据：${name} ${id}
+</form>
+</body>
+</html>
