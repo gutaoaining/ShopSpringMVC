d23cac883064875a555225c3fb7e447d80d860a6
gutao <gutaoai@126.com>
2015-10-08 14:51:32 +0800

    gutao

--- src/main/java/com/gt/listener/Initlistener.java
+++ src/main/java/com/gt/listener/Initlistener.java
@@ -3,12 +3,28 @@ package com.gt.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-public class Initlistener implements ServletContextListener {
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import com.gt.services.base.ProductServiceI;
+
+public class Initlistener implements ServletContextListener {
+	private ProductServiceI productService = null;
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
-
+		//解决方案一:通过直接拿配置文件来获得service,但是并不好，因为加载了两次配置文件
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml","classpath:spring-hibernate.xml");
+//      productService = (ProductServiceI)applicationContext.getBean("productService");
+//      System.out.println("-------------自己的监听器："+productService);
+	    //解决方案二:项目在启动时把spring配置文件通过配置加载，存储了servletContext,只需要获取就可以了
+		  //这里key值有点长，不方便
+//		ApplicationContext applicationContext =(ApplicationContext)sce.getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+//		productService = (ProductServiceI)applicationContext.getBean("productService");
+//		System.out.println("---------productService:"+productService);
+		//解决方案三:利用spring提供的一个工具类
+		ApplicationContext applicationContext =(ApplicationContext)WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
+	    productService = (ProductServiceI)applicationContext.getBean("productService");
+	    System.out.println("----------productService:"+productService);
 	}
 
 	@Override
