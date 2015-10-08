067ea249fae83dbfd89defad8d35e5fdcbd33cbc
gutao <gutaoai@126.com>
2015-10-08 14:50:15 +0800

    gutao

---
+++ src/main/java/com/gt/listener/Initlistener.java
@@ -0,0 +1,20 @@
+package com.gt.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class Initlistener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
